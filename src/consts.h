005285f05a887feab4cdceb2ed055e33b9907598
wanglf <m@wanglf.xyz>
2025-03-08 08:35:57 +0800

    update

---
+++ src/consts.h
@@ -0,0 +1,39 @@
+#define TFT_CS 10
+#define TFT_MOSI 11
+#define TFT_SCLK 12
+#define TFT_MISO 13
+#define TFT_DC 8
+#define TFT_RST 3
+
+#define ALARM_BTN_PIN 4
+#define LEFT_BTN_PIN 5
+#define RIGHT_BTN_PIN 6
+#define LIGHT_BTN_PIN 7
+
+/*
+#define SPEED_PIN 47
+
+#define LEFT_CTL_PIN 42
+#define RIGHT_CTL_PIN 45
+#define LIGHT_CTL_PIN 39
+
+#define IIC_SDA 1
+#define IIC_SCL 2
+
+
+#define UART_RX 41
+#define UART_TX 40
+
+*/
+#define SPEED_PIN 38
+
+#define LEFT_CTL_PIN 35
+#define RIGHT_CTL_PIN 36
+#define LIGHT_CTL_PIN 37
+
+#define IIC_SDA 1
+#define IIC_SCL 2
+
+
+#define UART_RX 21
+#define UART_TX 20
